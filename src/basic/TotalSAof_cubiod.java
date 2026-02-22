a6fb88188e60a518d0a8561876b0ee12e182d532
Hemant Gupta <codewithemant@gmail.com>
2026-02-22 14:40:55 +0530

    Add files via upload

---
+++ src/basic/TotalSAof_cubiod.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class TotalSAof_cubiod {
+    static void main(String[] args) {
+        int l= 5;
+        int b= 6;
+        int h = 8;
+        int TSA = 2*((l*b)+ (b*h) + (h*l));
+        System.out.println(TSA);
+    }
+}
