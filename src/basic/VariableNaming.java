8791da0a43c5eb0607d3d741ef8f90cdbde206dc
hemant <codewithemant@gmail.com>
2026-02-22 14:55:07 +0530

    commit

---
+++ src/basic/VariableNaming.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class VariableNaming {
+    static void main(String[] args) {
+        int hemant = 100;
+        int gupta = 150;
+        System.out.println(hemant + gupta);
+        /* ! @ # $ % ^ & * ( ) _ + - = [ ] { } ? \ | : ; ' " . , > < ? ~  are not valid in variable
+        expect _ and $
+    */
+
+    }
+}
---
+++ src/basic/Volume_of_sphere.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Volume_of_sphere {
+    static void main(String[] args) {
+
+        double r = 3;
+      double  volume = (4*3.14*(r*r*r))/3;
+        System.out.println(volume);
+    }
+}
