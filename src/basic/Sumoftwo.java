61cc5b5b8c7ca94848f17db015ee2e67ec580a95
codewithemant <codewithemant@gmail.com>
2026-02-23 23:54:06 +0530

    commit

---
+++ src/basic/Sumoftwo.java
@@ -0,0 +1,17 @@
+package basic;
+
+
+import java.util.Scanner;
+
+public class Sumoftwo {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int gupta = sc.nextInt();
+        ystem.out.println("Enter second number:");
+        int rahul = sc.nextInt();
+        ystem.out.println("Enter third number:");
+        int karan = sc.nextInt();
+        System.out.println(gupta+rahul+karan);
+    }
+}
