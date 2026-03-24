3e2a953ef231fc13b451594560c8ba184a35eff8
codewithemant <codewithemant@gmail.com>
2026-03-24 23:19:40 +0530

    commit

--- src/basic/CharDataType.java
+++ src/basic/CharDataType.java
@@ -2,6 +2,7 @@ package basic;
 
 public class CharDataType {
     static void main(String[] args) {
+        // typecasting - means ek data type sa dusra ma jana ho jab use karta hai
        char ch = 'A';
         int x = ch; // implicit typecasting
         System.out.println(x);
